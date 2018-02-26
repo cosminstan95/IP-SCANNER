aa45ea2b3b177309007086ce58344015e582bc73
cosminstan95 <36852270+cosminstan95@users.noreply.github.com>
2018-02-26 16:28:25 +0200

    Add files via upload

---
+++ Program TEST.py
@@ -0,0 +1,21 @@
+
+question = raw_input("Cum te cheama?")
+if question == "George":
+    print "Nu te cred"
+else:
+    print "Nu te las sa intri"
+age = raw_input("Cati ani ai?")
+if age == "30":
+    print "Esti mic copile"
+    print "Resetati programul pentru a introduce un alt nume";
+elif question == "Cosmin":
+    print "El este regele"
+kg = raw_input("Cate kilograme ai?")
+if kg == "65":
+    print "Esti pe moarte"
+
+raw_input()
+
+
+
+
